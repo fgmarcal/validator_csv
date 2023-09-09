de24d60e8a33fbe3a165e6d63d8b8cd091ae2d0c
Felipe Garcia Marçal <90734397+fgmarcal@users.noreply.github.com>
2023-09-08 22:56:40 -0300

    update frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App:React.FC=()=> {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home:React.FC = (props)=>{
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Home;
